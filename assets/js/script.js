fea5ecc9118fe7e4f5d796a1378408d64dcef474
kevinmcsherry <kevinmcsherry01@gmail.com>
2022-09-12 08:28:50 +0000

    Added reset race button to start the race again

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,22 +12,26 @@ let car3 = document.getElementById("car_red");
  }
     
 
-    function startRaceCar1() {
-        car1.style.position = "relative";
-        car1.style.transform = "translateX(550px)";
-        car1.style.transition=" all 4s"
+function startRaceCar1() {
+    car1.style.position = "relative";
+    car1.style.transform = "translateX(550px)";
+    car1.style.transition=" all 4s"
+}
+
+function startRaceCar2() {
+    car2.style.position = "relative";
+    car2.style.transform = "translateX(550px)";
+    car2.style.transition=" all 6s"
+}
+
+function startRaceCar3() {
+    car3.style.position = "relative";
+    car3.style.transform = "translateX(550px)";
+    car3.style.transition=" all 7s"
     }
 
-    function startRaceCar2() {
-        car2.style.position = "relative";
-        car2.style.transform = "translateX(550px)";
-        car2.style.transition=" all 6s"
-    }
-
-    function startRaceCar3() {
-        car3.style.position = "relative";
-        car3.style.transform = "translateX(550px)";
-        car3.style.transition=" all 7s"
-    }
+function resetRace(){
+    window.location.reload();
+}
 
 
