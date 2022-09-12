c87720d34f2a9f26a956a53253a57595f0b636e7
kevinmcsherry <kevinmcsherry01@gmail.com>
2022-09-12 08:10:32 +0000

    Added new common function to race all three cars simutanenously

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,45 +2,32 @@
 let car1 = document.getElementById("car_green");
 let car2 = document.getElementById("car_blue");
 let car3 = document.getElementById("car_red");
-/**car1.addEventListener("click", move1);
-car2.addEventListener("click", move2);
-car3.addEventListener("click", move3);**/
 
 
 
-/**function move1(e) {
-   carX += 550;
-   car1.style.position = "relative";
-   car1.style.transform = "translateX(" + carX + "px)";
-}
-function move2(e) {
-    carX += 550;
-    car2.style.position = "relative";
-    car2.style.transform = "translateX(" + carX + "px)";
+ function startRace() {
+    startRaceCar1();
+    startRaceCar2();
+    startRaceCar3();
  }
- function move3(e) {
-    carX += 550;
-    car3.style.position = "relative";
-    car3.style.transform = "translateX(" + carX + "px)";
- } **/
     
 
     function startRaceCar1() {
         car1.style.position = "relative";
         car1.style.transform = "translateX(550px)";
-        car1.style.transition=" all 2s"
+        car1.style.transition=" all 4s"
     }
 
     function startRaceCar2() {
         car2.style.position = "relative";
         car2.style.transform = "translateX(550px)";
-        car2.style.transition=" all 2s"
+        car2.style.transition=" all 6s"
     }
 
     function startRaceCar3() {
         car3.style.position = "relative";
         car3.style.transform = "translateX(550px)";
-        car3.style.transition=" all 2s"
+        car3.style.transition=" all 7s"
     }
 
 
